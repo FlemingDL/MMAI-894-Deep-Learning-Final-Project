1333a9a3cfa4ff56c115069a8a9756310fdcdc93
rashaad-queensu <rYwzyn-wynku9-xedcud>
2020-02-22 15:05:39 -0500

    added support for xception network

--- train.py
+++ train.py
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from joke.jokes import *
 import markdown_strings as ms
+import Xception_PyTorch.xception as xception
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='data',
@@ -272,6 +274,19 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
         Be careful, expects (299,299) sized images and has auxiliary output
         """
         model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 299
+    elif model_name == "xception":
+        """ Xception
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = xception(pretrained=use_pretrained)
         set_parameter_requires_grad(model_ft, feature_extract)
         # Handle the auxilary net
         num_ftrs = model_ft.AuxLogits.fc.in_features
