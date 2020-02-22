7de8ffd292dfb9c326100f40c14a73dc88d47550
rashaad-queensu <rYwzyn-wynku9-xedcud>
2020-02-22 15:57:09 -0500

    bug fix in initializing xception

--- train.py
+++ train.py
@@ -286,10 +286,8 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
         """ Xception
         Be careful, expects (299,299) sized images and has auxiliary output
         """
-        model_ft = xception.xception(pretrained=use_pretrained)
+        model_ft = xception.xception(pretrained=use_pretrained, num_classes=num_classes)
         set_parameter_requires_grad(model_ft, feature_extract)
-        num_ftrs = model_ft.classifier.in_features
-        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
         input_size = 299
 
     else:
