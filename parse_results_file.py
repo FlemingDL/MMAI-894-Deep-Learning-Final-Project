d80c5652a1dc1a956948ee0a60648cb7ce45cd38
rashaad-queensu <rYwzyn-wynku9-xedcud>
2020-02-23 16:00:08 -0500

    created a file to parse the results returned from stanford cars database server

---
+++ parse_results_file.py
@@ -0,0 +1,24 @@
+import argparse
+import scipy.io as spio
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file_path', help="The path file of the response file")
+
+if __name__ == '__main__':
+
+    # Collect arguments from command-line options
+    args = parser.parse_args()
+
+    if args.file_path is None:
+        print('A file to parse must be provided.  Enter \'python parse_results_file.py --file_path <Path to file>\'')
+        exit()
+
+    file_data = spio.loadmat(args.file_path)
+
+    print('Exporting confusion matrix to confusion_matrix.csv')
+    cm = file_data['confusion_matrix']
+    df = pd.DataFrame(cm)
+    df.to_csv('confusion_matrix.csv', index=False, header=False)
+
+    print('The reported accuracy was: {}'.format(file_data['accuracy'][0][0]))
