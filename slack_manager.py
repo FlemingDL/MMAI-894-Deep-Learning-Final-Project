ecabb57f06ca8cbdce1ad8e9c4ea3d836c7ab05f
rashaad-queensu <rYwzyn-wynku9-xedcud>
2020-02-26 18:21:22 -0500

    created a slack_manager.py for use in all files (will refactor later)

---
+++ slack_manager.py
@@ -0,0 +1,59 @@
+import slack
+import logging
+import ssl
+
+
+class SlackManager:
+    client = None
+    slack_channel = ""
+
+    def __init__(self, channel):
+        self.slack_channel = channel
+
+    def setup(self, slack_api_token):
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE
+        self.client = slack.WebClient(token=slack_api_token, ssl=ssl_context)
+
+    def post_slack_message(self, message, response=None):
+        if self.client is not None:
+            if response is None:
+                try:
+                    response = self.client.chat_postMessage(channel=self.slack_channel, text=message)
+                except:
+                    logging.info('Error posting message to slack')
+            else:
+                try:
+                    response = self.client.chat_update(channel=response['channel'], ts=response['ts'], text=message)
+                except:
+                    logging.info('Error posting message to slack')
+
+            return response
+
+    def post_slack_file(self, file_name, response=None):
+        if self.client is not None:
+            if response is None:
+                try:
+                    response = self.client.files_upload(channels=self.slack_channel, file=file_name, filename=file_name)
+                except:
+                    logging.info('Error uploading file to slack')
+            else:
+                self.delete_slack_file(response)
+                try:
+                    response = self.client.files_upload(channels=self.slack_channel, file=file_name, filename=file_name)
+                except:
+                    logging.info('Error uploading file to slack')
+
+            return response
+
+    def delete_slack_message(self, response):
+        if self.client is not None:
+            try:
+                self.client.chat_delete(channel=response['channel'], ts=response['ts'])
+            except:
+                logging.info('Error deleting message')
+
+    def delete_slack_file(self, response):
+        if self.client is not None:
+            self.client.files_delete(file=response['file']['id'])
